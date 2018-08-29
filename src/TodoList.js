23381ebc7ee126c6a9e7928e28f12236462da56c
wwt <648128145@qq.com>
2018-08-29 22:23:08 +0800

    组件拆分

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,7 @@
-import React, {Component, Fragment} from 'react'
-import './style.css'
+import React, {Component, Fragment} from 'react';
+import './style.css';
+import TodoItem from './TodoItem';
+
 class TodoList extends Component {
     constructor(props) {
         //继承
@@ -28,12 +30,13 @@ class TodoList extends Component {
                     {
                         this.state.list.map((item, index) => {
                             return (
-                            <li key={index} 
-                            onClick={this.handleItemDelete.bind(this, index)}
-                            dangerouslySetInnerHTML={{__html: item}}
-                            >
-                            
-                            </li>
+								<div>
+									<TodoItem 
+									content={item} 
+									index={index}
+									deleteItem={this.handleItemDelete.bind(this)}
+									/>
+								</div>
                         )
                         })
                     }
