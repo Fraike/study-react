76ab42ccc652f447cd01e6c3c005e06f6aad417c
wwt <648128145@qq.com>
2018-09-01 11:32:28 +0800

    添加prosp校验

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class TodoItem extends Component {
     constructor(props){
@@ -6,10 +7,10 @@ class TodoItem extends Component {
         this.handleClick = this.handleClick.bind(this)
     }
     render () {
-        const { content } = this.props ;
+        const { content, test } = this.props ;
         return (
             <div onClick={this.handleClick}>
-                {content}
+               {test} = {content}
             </div>
         )
     }
@@ -17,5 +18,15 @@ class TodoItem extends Component {
         const { deleteItem, index } = this.props;
         deleteItem(index);
     }
+}
+
+TodoItem.propTypes = {
+    test: PropTypes.string.isRequired,
+    content: PropTypes.arrayOf(PropTypes.string, PropTypes.number),
+    deleteItem: PropTypes.func,
+    index: PropTypes.number
+}
+TodoItem.defaultProps = {
+    test: 'hello'
 }
 export default TodoItem;
