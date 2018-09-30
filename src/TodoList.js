a706eeaaf74db60f8d04755dede9146ca9b02ae3
wwt <648128145@qq.com>
2018-09-30 16:09:14 +0800

    updadte

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,32 +1,57 @@
 import React,{ Component } from 'react';
 import 'antd/dist/antd.css'; 
 import { Input,Button,List   } from 'antd';
+import store from './store';
 
-const data = [
-    'Racing car sprays burning fuel into crowd.',
-    'Japanese princess to wed commoner.',
-    'Australian walks 100km after outback crash.',
-    'Man charged over missing wedding girl.',
-    'Los Angeles battles huge wildfires.',
-  ];
   
 
 class TodoList extends Component {
+    constructor(props) {
+        super(props);
+        this.state = store.getState();
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleStoreChange = this.handleStoreChange.bind(this);
+        this.handleBtnClick = this.handleBtnClick.bind(this);
+        store.subscribe(this.handleStoreChange)
+    }
+
     render() {
         return (
             <div style={{marginTop:'10px',marginLeft:'10px'}}>
                 <div>
-                    <Input placeholder="请输入" style={{width:'300px',marginRight:'10px'}}></Input>
-                    <Button type="primary">提交</Button>
+                    <Input 
+                    value={this.state.inputValue} 
+                    placeholder="请输入" 
+                    style={{width:'300px',marginRight:'10px'}}
+                    onChange={this.handleInputChange}
+                    ></Input>
+                    <Button type="primary" onClick={this.handleBtnClick}>提交</Button>
                 </div>
                 <List
                     bordered
-                    dataSource={data}
+                    dataSource={this.state.list}
                     renderItem={item => (<List.Item>{item}</List.Item>)}
                     style={{marginTop: '10px',width: '300px'}}
                 />
             </div>
         )
+    }
+    handleInputChange(e) {
+        const action = {
+            type: 'change_input_value',
+            value: e.target.value
+        };
+        store.dispatch(action)
+
+    }
+    handleStoreChange() {
+        this.setState(store.getState())
+    }
+    handleBtnClick() {
+        const action = {
+            type: 'add_todo_item'
+        };
+        store.dispatch(action)
     }
 }
 export default TodoList;
